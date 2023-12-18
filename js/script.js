ad41fcf1e18046de1c3e7277946372e74a94e44d
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-18 14:00:11 +0100

    added discount formula

--- js/script.js
+++ js/script.js
@@ -8,6 +8,30 @@ const chilometri = prompt('Quanti chilometri devi percorrere ?', '10');
 
 // Chidere all'utente la sua età
 
-const eta = prompt('Quanti anni hai ?', '66');
+const eta = parseInt(prompt('Quanti anni hai ?', '66'));
 
 console.log(chilometri, eta);
+
+
+//Creo costante prezzo biglietto per chilometro
+const perkm = 0.21 ;
+
+
+//calcolo il prezzo del biglietto
+const prezzobiglietto = chilometri * perkm ;
+
+console.log(prezzobiglietto);
+
+//Sconto del 20%
+
+const sconto20 = (20 * prezzobiglietto) / 100 ;
+
+console.log(sconto20);
+
+//Sconto del 40%
+
+const sconto40 = (40 * prezzobiglietto) / 100 ;
+
+console.log(sconto40);
+
+
