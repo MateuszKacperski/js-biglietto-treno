a9ddab9dc807b8bd11f2435ff8695c97365c7bcc
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-18 13:50:23 +0100

    added prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+
+
+
+//Chiedere all'utente i chilometri da percorrere
+
+const chilometri = prompt('Quanti chilometri devi percorrere ?', '10');
+
+// Chidere all'utente la sua età
+
+const eta = prompt('Quanti anni hai ?', '66');
+
+console.log(chilometri, eta);
